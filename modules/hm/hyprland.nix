5001e6c2b22600ce563ff9dc4137491c7fd0d22e
false <false>
2026-01-14 21:26:17 +0530

    added hyprland.nix

--- modules/hm/hyprland.nix
+++ modules/hm/hyprland.nix
@@ -6,7 +6,15 @@
     extraConfig = ''
       input {
         kb_options = caps:swapescape
-      }
+	touchpad {
+	    natural_scroll = true
+	    tap_button_map = lmr
+	}
+	}
+	misc {
+	    mouse_move_enables_dpms = true
+	    middle_click_paste = false
+	}
     '';
   };
 }
