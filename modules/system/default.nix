29e707515a23a8c2c3fda0dcb54ed278f76b6b98
false <false>
2026-01-14 21:25:22 +0530

    added zen-browser, brave and keepassxc

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -5,7 +5,10 @@
     # ./example.nix - add your modules here
   ];
 
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
+	brave,
+	zen-browser,
+	keepassxc
     # pkgs.vscode - hydenix's vscode version
     # pkgs.userPkgs.vscode - your personal nixpkgs version
   ];
