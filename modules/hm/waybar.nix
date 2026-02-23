684008bcf4e0a0f0699b86ded232d2fd36fc3b06
false <false>
2026-02-23 10:02:15 +0530

    chore: Try fixing waybar config for idle inhibitor

--- modules/hm/waybar.nix
+++ modules/hm/waybar.nix
@@ -1,3 +1,37 @@
-{
+{ 
+  home.file.".local/share/waybar/modules/idle_inhibitor.jsonc".text = ''
+  {
+    "idle_inhibitor": {
+      "format": "{icon}",
+      "rotate": 0,
+      "start-activated": true,
+      "format-icons": {
+        "activated": "\udb80\udd76",
+        "deactivated": "\udb81\udeca"
+      },
+      "justify": "center",
+      "tooltip": "true",
+      "tooltip-format-activated": "<span foreground='#98c379'>\udb80\udd76 Caffeine Mode Active</span>\nPrevents system from going to sleep",
+      "tooltip-format-deactivated": "<span foreground='#e06c75'>\udb81\udeca Caffeine Mode Inactive</span>\nSystem will follow normal power settings"
+    }
+  }
+  '';
+  home.file.".config/waybar/modules/idle_inhibitor.jsonc".text = ''
+    {
+      "idle_inhibitor": {
+        "format": "{icon}",
+        "rotate": 0,
+        "start-activated": true,
+        "format-icons": {
+          "activated": "\udb80\udd76",
+          "deactivated": "\udb81\udeca"
+        },
+        "justify": "center",
+        "tooltip": "true",
+        "tooltip-format-activated": "<span foreground='#98c379'>\udb80\udd76 Caffeine Mode Active</span>\nPrevents system from going to sleep",
+        "tooltip-format-deactivated": "<span foreground='#e06c75'>\udb81\udeca Caffeine Mode Inactive</span>\nSystem will follow normal power settings"
+      }
+    }
+  '';
   programs.waybar.settings.modules."idle_inhibitor"."start-activated" = true;
 }
